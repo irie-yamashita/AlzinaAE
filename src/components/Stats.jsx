53610ed36dd8d8363b60db4ce69ff5b62d61c4bd
irieyamashita <irie.yamashita.7e8@itb.cat>
2025-10-13 13:57:22 +0200

    fix responsive stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,17 +1,17 @@
 function Stats() {
 
   return (
-    <div className="bg-primary flex justify-center items-center gap-24 h-72 mb-4">
+    <div className="bg-primary flex justify-center items-center h-32 gap-10 md:gap-24 md:h-72">
         <div className="flex flex-col text-center justify-center items-center">
             <p className="font-catallina text-[36px] text-secondary">14</p>
             <p className="font-catallina text-[16px] text-background">Equips Registrats</p>
         </div>
-        <div className="border h-24"></div>
+        <div className="bg-background h-20 md:h-24 size-0.5"></div>
         <div className="flex flex-col text-center justify-center items-center">
             <p className="font-catallina text-[36px] text-secondary">450+</p>
             <p className="font-catallina text-[16px] text-background">Jugadors/res</p>
         </div>
-        <div className="border h-24"></div>
+        <div className="bg-background h-20 md:h-24 size-0.5"></div>
         <div className="flex flex-col text-center justify-center items-center">
             <p className="font-catallina text-[36px] text-secondary">130+</p>
             <p className="font-catallina text-[16px] text-background">Premis Guanyats</p>
