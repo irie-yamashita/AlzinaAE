2b6b8ecde012dd1edfc64fd8007fefe66a8d9a79
irieyamashita <irie.yamashita.7e8@itb.cat>
2026-02-28 21:55:25 +0100

    update styles responsive Calendar

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -2,22 +2,20 @@ import dayjs from "dayjs";
 
 function Match({ match }) {
     return (
-        <a href="">
-            <article className="match">
-                <div className="teams-container">
-                    <div className="team">
-                        <img className="logo" src={match.homeTeam.logo} alt={`${match.homeTeam.name} logo`} />
-                        <p>{match.homeTeam.name}</p>
-                    </div>
-                    <span>vs</span>
-                    <div className="team">
-                        <img className="logo" src={match.awayTeam.logo} alt={`${match.awayTeam.name} logo`} />
-                        <p>{match.awayTeam.name}</p>
-                    </div>
+        <article className="match">
+            <div className="teams-container">
+                <div className="team">
+                    <img className="logo" src={match.homeTeam.logo} alt={`${match.homeTeam.name} logo`} />
+                    <p>{match.homeTeam.name}</p>
                 </div>
-                <span className="hora">{dayjs(match.date).locale('es').format('HH:mm')}h</span>
-            </article>
-        </a>
+                <span className="vs">vs</span>
+                <div className="team">
+                    <img className="logo" src={match.awayTeam.logo} alt={`${match.awayTeam.name} logo`} />
+                    <p>{match.awayTeam.name}</p>
+                </div>
+            </div>
+            <span className="hora">{dayjs(match.date).locale('es').format('HH:mm')}h</span>
+        </article>
     )
 }
 
